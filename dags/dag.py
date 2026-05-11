a587723b6c55e212c9c3fa580fa9411fc5435f8a
YanNaing Htun <130727209+yannaingtun5555@users.noreply.github.com>
2026-05-11 07:53:48 +0000

    Add load module and create table to the pipeline

--- dags/dag.py
+++ dags/dag.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from utilis.broker_check import check_kafka_health
 from pipeline.extract import extract as extract_func
 from pipeline.transform import transform_and_stream
+from pipeline.load import load_data
+from pipeline.table_create import create_table
 
 @dag(
     dag_id="kafka_health",
@@ -19,18 +21,28 @@ def weather_pipeline():
         )
 
     @task
+    def table_check():
+        return create_table()
+    
+    @task
     def extract_task():
         return extract_func()   # ✔ correct
 
     @task
     def transform_task():
         return transform_and_stream()
+ 
+    @task
+    def load_task():
+        return load_data()
 
     check = run_kafka_check()
+    create_tab = table_check()
     extract_t = extract_task()
     transform_t = transform_task()
+    load_t = load_task()
 
-    check >> extract_t >> transform_t
+    check >> create_tab >> extract_t >> transform_t >> load_t
 
 
 dag = weather_pipeline()
--- pipeline/load.py
+++ pipeline/load.py
@@ -1,8 +1,36 @@
 import psycopg2
-
+from kafka import KafkaConsumer
 from pipeline.config import get_db_config
+import json
 
-def load_data(data_list):
+def load_data():
+    consumer = KafkaConsumer(
+        'transformed_topic',
+        bootstrap_servers=['kafka1:9092','kafka2:9092'],
+        value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+        auto_offset_reset='earliest',
+        group_id='weather_load',
+        consumer_timeout_ms= 10 * 1000
+
+    )
+    print("Kafka consumer initialized, waiting for messages...")
+    data_list = []
+    BATCH_SIZE = 4
+
+    for message in consumer:
+        data = message.value
+        data_list.append(data)
+
+        print(f"Received for loading: {data['city']} at {data['timestamp']}")
+
+        if len(data_list) >= BATCH_SIZE:
+            if load_database(data_list):
+                print(f"Loaded batch of {len(data_list)} records to database")
+                data_list.clear()
+            else:
+                print("Failed to load batch to database")
+
+def load_database(data_list):   
     conn = None
     try:
         conn = psycopg2.connect(**get_db_config())
@@ -16,15 +44,21 @@ def load_data(data_list):
                 ({', '.join(columns)})
                 VALUES ({placeholders})
                 ON CONFLICT (city, timestamp) DO NOTHING
-            """
+               """
             cur.execute(sql, data)
-        
+
+            # ✅ log what you're inserting
+            print(f"Inserted/Processed city: {data['city']} at {data['timestamp']}")
+
         conn.commit()
         cur.close()
-        return True
+        return True           
+# 
+            
     except psycopg2.Error as exc:
         print(f"Database error loading weather row: {exc}")
         raise  
+
     finally:
         if conn:
             conn.close()
